06ff1a19be573eef89b2bbd5f9f971f35db7b190
Roni Palmon <roni.palmon@post.runi.ac.il>
2024-01-19 16:41:59 +0200

    all

--- GameOfLife.java
+++ GameOfLife.java
@@ -12,8 +12,8 @@ public class GameOfLife {
 		//// Uncomment the test that you want to execute, and re-compile.
 		//// (Run one test at a time).
 		 /// test1(fileName);
-		 test2(fileName);
-		//// test3(fileName, 3);
+		 //test2(fileName);
+		 test3(fileName, 3);
 		//// play(fileName);
 	}
 	
@@ -96,8 +96,15 @@ public class GameOfLife {
 	// Uses the cellValue(board,i,j) function to compute the value of each 
 	// cell in the new board. Returns the new board.
 	public static int[][] evolve(int[][] board) {
-		//// Replace the following statement with your code.
-		return null;
+		int[][] newBoard = new int[board.length][board[0].length];
+		for (int i=1; i<board.length-1; i++){
+			for (int j=1; j<board[0].length-1; j++){
+				
+				newBoard[i][j]=cellValue(board, i, j);
+			}
+		}
+		
+		return newBoard;
 	}
 
 	// Returns the value that cell (i,j) should have in the next generation.
